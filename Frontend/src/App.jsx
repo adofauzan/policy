ab09acd02a2076f0425fa6de3f7949a76329f40a
unknown <fauzanputra1@gmail.com>
2025-02-08 11:04:32 +0700

    Fix alignment issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,19 +3,21 @@ import PolicyPage from "./component/PolicyPage";
 
 function App() {
   return (
-    <Box
-      sx={{
+    <div
+      style={{
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         minHeight: "100vh",
-        padding: 3,
+        padding: 20,
+        width: "100vw", // Ensures it takes full width
+        textAlign: "center", // Centers the title
       }}
     >
       <h1>Managemen Polis</h1>
       <PolicyPage />
-    </Box>
+    </div>
   );
 }
 
